31cd4ffc76416316deca95c03cb3afafd78afcb8
supersaiyansubtlety <supersaiyansubtlety@users.noreply.github.com>
2019-02-21 14:04:38 -0800

    file writing

--- main.cpp
+++ main.cpp
@@ -7,13 +7,25 @@
 //
 
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int consecutiveGCD(const int& left, const int& right);
-int avgConsecutiveGCD(const int& input);
+double avgConsecutiveGCD(const int& input);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ofstream consecutiveGCDdat, euclidsGCDdat;
+    consecutiveGCDdat.open("Consecutive GCD.dat");
+    euclidsGCDdat.open("Euclid's GCD.dat");
+    
+    for (int i = 1; i <= 100; i += 3)
+    {
+        consecutiveGCDdat << i << '\t' << avgConsecutiveGCD(i) << endl;
+    }
+    
+    consecutiveGCDdat.close();
+    euclidsGCDdat.close();
     return 0;
 }
 
@@ -36,7 +48,7 @@ int consecutiveGCD(const int& left, const int& right)
     return divisions;
 }
 
-int avgConsecutiveGCD(const int& input)
+double avgConsecutiveGCD(const int& input)
 {
     int totalDivs = 0;
     for (int i = 1; i <= input; i++)
