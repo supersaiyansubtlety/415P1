85b337d17a65b988bd17adefd7ddc1aa5cf0124e
Will <student@ssu-cs-dar28-167-218.sonoma.edu>
2019-02-27 12:44:02 -0800

    python with cones and av_consec

---
+++ 415P1py/main.py
@@ -0,0 +1,27 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+
+def consecutive_gcd(left, right):
+    divisions = 0
+
+    if left > right:
+        minimum = left
+    else:
+        minimum = right
+
+    for val in range(minimum, 0, -1):  # i = min; i > 0; i--)
+        if left % minimum == 0:
+            divisions += 1
+            if right % minimum == 0:
+                divisions += 1
+                return divisions
+
+    return divisions
+
+
+def avg_consecutive_gcd(in_val):
+    total_divs = 0
+    for val in range(1, in_val, 1):  # (int i = 1; i <= in_val; i++):
+        total_divs += consecutive_gcd(in_val, val)
+    return total_divs/in_val
