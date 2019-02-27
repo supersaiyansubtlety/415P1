c23101fff69b476854f2ace7535a8befe104ab56
Will <student@ssu-cs-dar28-167-218.sonoma.edu>
2019-02-27 13:02:07 -0800

    added consecutive stuff

--- 415P1/main.py
+++ 415P1/main.py
@@ -30,6 +30,29 @@ def main():
         mdavg_data.append(mdavg(i))
         consec_div_data.append(avg_consecutive_gcd(i))
 
+def consecutive_gcd(left, right):
+    divisions = 0
+
+    if left > right:
+        minimum = left
+    else:
+        minimum = right
+
+    for i in range(minimum, 0, -1):  # i = min; i > 0; i--)
+        if left % minimum == 0:
+            divisions += 1
+            if right % minimum == 0:
+                divisions += 1
+                return divisions
+
+    return divisions
+
+
+def avg_consecutive_gcd(in_val):
+    total_divs = 0
+    for i in range(1, in_val, 1):  # (int i = 1; i <= in_val; i++):
+        total_divs += consecutive_gcd(in_val, i)
+    return total_divs/in_val
 
 """
 int
